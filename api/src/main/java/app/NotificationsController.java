3ef06708730a65c30c732437e8d9914546d27849
ruifpedro <ruifilipe.rspedro@gmail.com>
2019-12-21 20:56:50 +0000

    api: - implements the registerWebhook feature - implements the webhook threshold messages handling

--- api/src/main/java/app/NotificationsController.java
+++ api/src/main/java/app/NotificationsController.java
@@ -3,31 +3,50 @@ package app;
 import app.config.NotificationsCtrlConfig;
 import app.models.WebHookMsg;
 import com.google.common.base.Preconditions;
+import com.google.gson.Gson;
 import models.ThresholdMsg;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Collections;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @RestController
 @RequestMapping("/notifications")
 public class NotificationsController {
+
+	private Gson gson = new Gson();
+	private final HttpClient httpClient = HttpClient.newBuilder()
+													.version(HttpClient.Version.HTTP_2)
+													.build();
 
 	private Map<String, String> hooks = new ConcurrentHashMap<>();
 
 	private final String topicName;
 	private KafkaConsumer<String, ThresholdMsg> consumer;
 
+	private Runnable webHookHandler;
+	private AtomicBoolean running = new AtomicBoolean(true);
+	private int timeout;
+
 	@Autowired
 	public NotificationsController(NotificationsCtrlConfig notificationsCtrlConfig) {
 		this.topicName = notificationsCtrlConfig.getTopicName();
+		this.timeout = notificationsCtrlConfig.getTimeout();
 
 		var properties = notificationsCtrlConfig.getConsumer();
 		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
@@ -37,44 +56,54 @@ public class NotificationsController {
 		// TODO - fix topicName being a string not a list
 		this.consumer.subscribe(Collections.singleton(this.topicName));
 
-		//TODO - add to config
-		var timeout = 100;
 
 		// TODO - change into sys shutdown hook
-		var running = true;
-
-		// TODO - maybe extract into a class
-		new Runnable() {
-
-			@Override
-			public void run() {
-				while (running) {
-					consumer.poll(timeout).forEach(record -> {
-						//TODO - implement business logic
-					});
-
-				}
+		webHookHandler = () -> {
+			while (running.get()) {
+				consumer.poll(timeout).forEach(record ->
+						hooks.values().forEach(url -> {
+							HttpRequest request = HttpRequest.newBuilder()
+															 .POST(recordToJson(record))
+															 .uri(URI.create(url))
+															 .setHeader("User-Agent", "SportsBook WH Bot")
+															 .header("Content-Type", "application/json")
+															 .build();
+							try {
+								HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+								//TODO - change into logging
+								System.out.println(response.statusCode());
+							} catch (IOException e) {
+								e.printStackTrace();
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+						}));
 			}
 		};
+		Thread webHookHandlerThread = new Thread(webHookHandler);
+		webHookHandlerThread.start();
+	}
+
+	private HttpRequest.BodyPublisher recordToJson(ConsumerRecord<String, ThresholdMsg> record) {
+		var msg = gson.toJson(record.value());
+		return HttpRequest.BodyPublishers.ofString(msg);
 	}
 
 	@PostMapping(path = "registerWebhook")
 	@ResponseStatus(HttpStatus.OK)
-	public void registerWebhook(@RequestBody WebHookMsg webHookMsg) {
+	public ResponseEntity<String> registerWebhook(@RequestBody WebHookMsg webHookMsg) {
 		Preconditions.checkNotNull(webHookMsg);
-
-		//TODO implement webhook dispatcher interaction
 
 		// send it back to the user
 		var hookID = UUID.randomUUID().toString();
 
 		// register hook
-		if (hooks.containsValue(webHookMsg.getUrl())){
-			//hook URL already registed
-			//TODO - send an error message back
-		}else{
+		if (hooks.containsValue(webHookMsg.getUrl())) {
+			//hook URL already registered
+			return new ResponseEntity<>("URL already registered", HttpStatus.NOT_ACCEPTABLE);
+		} else {
 			hooks.put(hookID, webHookMsg.getUrl());
+			return new ResponseEntity<>("Hook registered", HttpStatus.OK);
 		}
-
 	}
 }
--- api/src/main/java/app/config/NotificationsCtrlConfig.java
+++ api/src/main/java/app/config/NotificationsCtrlConfig.java
@@ -10,6 +10,15 @@ import java.util.Properties;
 public class NotificationsCtrlConfig {
 	private Properties consumer;
 	private String topicName;
+	private int timeout;
+
+	public int getTimeout() {
+		return timeout;
+	}
+
+	public void setTimeout(int timeout) {
+		this.timeout = timeout;
+	}
 
 	public Properties getConsumer() {
 		return consumer;
@@ -32,6 +41,7 @@ public class NotificationsCtrlConfig {
 		return "NotificationsCtrlConfig{" +
 				"consumer=" + consumer +
 				", topicName='" + topicName + '\'' +
+				", timeout=" + timeout +
 				'}';
 	}
 }
